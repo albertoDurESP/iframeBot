1b0daa2fa2d33f929757f858db343266bcba5574
James Hopkins <James.Hopkins@volume.co.uk>
2023-03-28 12:15:19 +0100

    Added styles and initial set up

---
+++ script.js
@@ -0,0 +1,11 @@
+//chat toggle
+function toggleChat() {
+    var element = document.getElementById("chatContainer");
+    element.classList.toggle("active");
+  }
+
+//popup
+var popup = document.getElementById("cookie-popup");
+function closePopup() {
+  popup.style.display = "none";
+}
