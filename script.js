15e93a68e1816a6272e5dacbf55d3f00cb5acb92
James Hopkins <James.Hopkins@volume.co.uk>
2023-03-30 12:47:03 +0100

    Added control for a pop up advert

--- script.js
+++ script.js
@@ -9,3 +9,44 @@ var popup = document.getElementById("cookie-popup");
 function closePopup() {
   popup.style.display = "none";
 }
+
+//advert timer
+		var countdownTime = 15;
+		var timerElement = document.getElementById("timer");
+		timerElement.innerHTML = countdownTime;
+		var timerCheckbox = document.getElementById("timerCheckbox");
+		var countdownInterval;
+
+		function startCountdown() {
+			countdownInterval = setInterval(function() {
+				countdownTime--;
+				timerElement.innerHTML = countdownTime;
+				if (countdownTime === 0) {
+					clearInterval(countdownInterval);
+					showAdvert();
+				}
+			}, 1000);
+		}
+
+		function stopCountdown() {
+			clearInterval(countdownInterval);
+      countdownTime = 15;
+		}
+
+		timerCheckbox.addEventListener("change", function() {
+			if (this.checked) {
+				startCountdown();
+			} else {
+				stopCountdown();
+			}
+		});
+
+//advert
+var advert = document.getElementById("advert-popup");
+function showAdvert() {
+  advert.style.display = "block";
+}
+function closeAdvert() {
+  advert.style.display = "none";
+}
+
